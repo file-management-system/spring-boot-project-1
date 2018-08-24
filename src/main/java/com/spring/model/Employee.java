bb7207a5f9dd70a6bf58e7a7063c355c73496240
Sipun123 <40847555+Sipun123@users.noreply.github.com>
2018-08-24 18:00:42 +0530

    added new spring-boot project with querybuilder class

---
+++ src/main/java/com/spring/model/Employee.java
@@ -0,0 +1,49 @@
+package com.spring.model;
+
+public class Employee {
+	private int empId;
+	private String name;
+	private String email;
+	private int phoneNumber;
+	private int departmentId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+}
